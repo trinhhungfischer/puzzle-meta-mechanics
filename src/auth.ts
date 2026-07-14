68604f5c84f52be202a59468bad04d77501e1c08
Trinh Quang Hung <hungtq@zitga.com.vn>
2026-07-14 14:07:50 +0700

    fix(admin): explicitly pass secret to NextAuth config to fix MissingSecret error

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
   providers: [Google],
   pages: {
     signIn: "/admin/login",
